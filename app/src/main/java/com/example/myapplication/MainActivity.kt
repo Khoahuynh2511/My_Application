0cc0af12295b436d2ecf44d0fe9ae24bafdf9841
Khoa <dangkhoahuynh2511@gmail.com>
2025-02-26 12:00:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Find the VideoView class by its id
+        val videoView = findViewById<VideoView>(R.id.testView)
+
+        // Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        // Specify the location of media file
+        val uri: Uri = Uri.parse(
+            "android.resource://" + packageName + "/" + R.raw.sample_video
+        )
+
+        // Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
+
+
+}
